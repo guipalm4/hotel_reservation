c5dc2b6c9aeff2b43dd1b7daff49c3081f35a7e5
guipalm4 <gomes.lmc@gmail.com>
2024-10-25 14:39:19 -0300

    refac: remove test folder and fix coverage

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,18 @@
 module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "jsdom",
-	moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
-	setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+	roots: ["<rootDir>/src"],
+	testMatch: ["**/?(*.)+(test|spec).+(ts|tsx|js)"],
+	collectCoverageFrom: [
+		"src/**/*.{ts,tsx}",
+		"!src/**/*.d.ts",
+		"!src/**/index.ts",
+	],
+	coverageDirectory: "<rootDir>/coverage",
+	coverageReporters: ["json", "lcov", "text", "clover"],
+	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+	transform: {
+		"^.+.(ts|tsx)$": "ts-jest",
+	},
+	setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
 };
